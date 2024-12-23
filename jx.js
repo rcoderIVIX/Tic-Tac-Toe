3ed42fcf7a93e9ae91e2e1a997f37df9e680b3a4
rcoderIVIX <rcoder0904@gmail.com>
2024-12-22 22:44:57 -0600

    Updated some colors

--- jx.js
+++ jx.js
@@ -47,56 +47,56 @@ gameBoardObject.gameBoard[choice6] = player2.marker;
     if (gameBoardObject.gameBoard[0] == "X" && gameBoardObject.gameBoard[3] == "X" && gameBoardObject.gameBoard[6] == "X"){
 
     winningLabel.textContent = playerName1.value + " Wins!"
-    winningLabel.style.color = "red"
+    winningLabel.style.color = "black"
     console.log(gameBoardObject.gameBoard)
     yesWinner = 1;
     
     } 
     if(gameBoardObject.gameBoard[1] == "X" && gameBoardObject.gameBoard[4] == "X" && gameBoardObject.gameBoard[7] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[2] == "X" && gameBoardObject.gameBoard[5] == "X" && gameBoardObject.gameBoard[8] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     if (gameBoardObject.gameBoard[0] == "X" && gameBoardObject.gameBoard[1] == "X" && gameBoardObject.gameBoard[2] == "X"){
     
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     
     } 
     if(gameBoardObject.gameBoard[3] == "X" && gameBoardObject.gameBoard[4] == "X" && gameBoardObject.gameBoard[5] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[6] == "X" && gameBoardObject.gameBoard[7] == "X" && gameBoardObject.gameBoard[8] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[0] == "X" && gameBoardObject.gameBoard[4] == "X" && gameBoardObject.gameBoard[8] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[2] == "X" && gameBoardObject.gameBoard[4] == "X" && gameBoardObject.gameBoard[6] == "X"){
         winningLabel.textContent = playerName1.value + " Wins!"
-        winningLabel.style.color = "red"
+        winningLabel.style.color = "black"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
@@ -105,56 +105,56 @@ gameBoardObject.gameBoard[choice6] = player2.marker;
     if (gameBoardObject.gameBoard[0] == "O" && gameBoardObject.gameBoard[3] == "O" && gameBoardObject.gameBoard[6] == "O"){
     
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     
     } 
     if(gameBoardObject.gameBoard[1] == "O" && gameBoardObject.gameBoard[4] == "O" && gameBoardObject.gameBoard[7] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[2] == "O" && gameBoardObject.gameBoard[5] == "O" && gameBoardObject.gameBoard[8] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     if (gameBoardObject.gameBoard[0] == "O" && gameBoardObject.gameBoard[1] == "O" && gameBoardObject.gameBoard[2] == "O"){
     
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     
     } 
     if(gameBoardObject.gameBoard[3] == "O" && gameBoardObject.gameBoard[4] == "O" && gameBoardObject.gameBoard[5] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[6] == "O" && gameBoardObject.gameBoard[7] == "O" && gameBoardObject.gameBoard[8] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[0] == "O" && gameBoardObject.gameBoard[4] == "O" && gameBoardObject.gameBoard[8] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
     
     if(gameBoardObject.gameBoard[2] == "O" && gameBoardObject.gameBoard[4] == "O" && gameBoardObject.gameBoard[6] == "O"){
         winningLabel.textContent = playerName2.value + " Wins!"
-        winningLabel.style.color = "blue"
+        winningLabel.style.color = "white"
         console.log(gameBoardObject.gameBoard)
         yesWinner = 1;
     }
@@ -193,42 +193,42 @@ let displayLogic = {
                     
                     button.textContent = player1.marker;
 
-                    button.style.color = "red";
+                    button.style.color = "black";
                 }
                 else if(p1choice1 !== "" && p2choice1 === "" && p1choice2 === "" && p2choice2 === "" && p1choice3 === "" && p2choice3 === ""){
                     p2choice1 = parseInt(button.textContent);
 
                     button.textContent = player2.marker;
 
-                    button.style.color = "blue";
+                    button.style.color = "white";
                 }
                 else if(p1choice1 !== "" && p2choice1 !== "" && p1choice2 === "" && p2choice2 === "" && p1choice3 === "" && p2choice3 === ""){
                     p1choice2 = parseInt(button.textContent);
 
                     button.textContent = player1.marker;
 
-                    button.style.color = "red";
+                    button.style.color = "black";
                 }
                 else if(p1choice1 !== "" && p2choice1 !== "" && p1choice2 !== "" && p2choice2 === "" && p1choice3 === "" && p2choice3 === ""){
                     p2choice2 = parseInt(button.textContent);
 
                     button.textContent = player2.marker;
 
-                    button.style.color = "blue";
+                    button.style.color = "white";
                 }
                 else if(p1choice1 !== "" && p2choice1 !== "" && p1choice2 !== "" && p2choice2 !== "" && p1choice3 === "" && p2choice3 === ""){
                     p1choice3 = parseInt(button.textContent);
 
                     button.textContent = player1.marker;
 
-                    button.style.color = "red";
+                    button.style.color = "black";
                 }
                 else if(p1choice1 !== "" && p2choice1 !== "" && p1choice2 !== "" && p2choice2 !== "" && p1choice3 !== "" && p2choice3 === ""){
                     p2choice3 = parseInt(button.textContent);
 
                     button.textContent = player2.marker;
 
-                    button.style.color = "blue";
+                    button.style.color = "white";
                 }
 
 
